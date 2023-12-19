36fd5c498fc5452ef973d07e4a0dee1c64598f20
clindoso <102795325+clindoso@users.noreply.github.com>
2023-12-19 11:10:06 +0100

    Add language argument to aligner

--- gpt-project/_data/scripts/aligner.py
+++ gpt-project/_data/scripts/aligner.py
@@ -1,16 +1,30 @@
 import os
 import csv
+import argparse
+
+# Use this script to create a CSV file as a TM with English as source language
+# This script takes one argument, --lang, the target language of the TM
+
+# Create argument parser
+argparser = argparse.ArgumentParser()
+
+# Define argument
+argparser.add_argument("--lang", required=True, help="Specify the target language for the translation memory")
+
+# Parse arguments
+args = argparser.parse_args()
 
 # Read and return file content in a list
 def read_and_split_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
-            return [line.strip() for line in file if line.strip()]
+            return [line.strip() for line in file 
+                    if line.strip() and not (line.strip().startswith('<!--') and line.strip().endswith('-->'))]
     except Exception as e:
-        return f"Error reading {file_path}: {e}"
+        raise Exception(f"Error reading {file_path}: {e}")
 
 output_directory = "/Users/caio.lopes/Documents/GitHub/clindoso/gpt-project/_docs/tm/"
-output_filename = "en-de.csv"
+output_filename = "en-" + args.lang + ".csv"
 
 # Export to CSV
 def write_to_csv(file1_lines, file2_lines, source_filename, output_filename, output_directory):
@@ -21,7 +35,7 @@ def write_to_csv(file1_lines, file2_lines, source_filename, output_filename, out
         writer = csv.writer(csvfile)
 
         if csvfile.tell() == 0:
-            writer.writerow(['EN', 'DE', 'File'])
+            writer.writerow(['en', args.lang, 'file'])
 
         if len(file1_lines) == len(file2_lines):
             for line1, line2 in zip(file1_lines, file2_lines):
@@ -35,10 +49,10 @@ def extract_file_contents(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_path_en = os.path.join(root, file)
-            file_path_es = file_path_en.replace("_en", "_es")
-            if os.path.exists(file_path_es):
+            file_path_tl = file_path_en.replace("_en", "_" + args.lang)
+            if os.path.exists(file_path_tl):
                 file_content_en = read_and_split_file(file_path_en)
-                file_content_es = read_and_split_file(file_path_es)
-                write_to_csv(file_content_en, file_content_es, file, output_filename, output_directory)
+                file_content_tl = read_and_split_file(file_path_tl)
+                write_to_csv(file_content_en, file_content_tl, file, output_filename, output_directory)
 
 extract_file_contents("/Users/caio.lopes/Documents/GitHub/clindoso/gpt-project/_docs/_en/")
