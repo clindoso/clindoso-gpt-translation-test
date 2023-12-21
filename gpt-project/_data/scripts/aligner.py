f5488b127fdd0c322a95d93a4a46383b784e32fc
clindoso <102795325+clindoso@users.noreply.github.com>
2023-12-21 10:14:22 +0100

    Revert last change in alignment

--- gpt-project/_data/scripts/aligner.py
+++ gpt-project/_data/scripts/aligner.py
@@ -133,13 +133,13 @@ def sentence_splitter(file1_lines, file2_lines):
         for i in range(len(file1_lines)):
             # Replace patterns in file1_lines
             line1 = file1_lines[i]
-            line1 = line1.replace(etc_pattern, "{PLACEHOLDER_ETC}")
+            line1 = etc_pattern.sub("{PLACEHOLDER_ETC}", line1)
             for pattern, placeholder in patterns.items():
                 line1 = line1.replace(pattern, placeholder)
 
             # Replace patterns in file2_lines
             line2 = file2_lines[i]
-            line2 = line2.replace(etc_pattern, "{PLACEHOLDER_ETC}")
+            line2 = etc_pattern.sub("{PLACEHOLDER_ETC}", line2)
             for pattern, placeholder in patterns.items():
                 line2 = line2.replace(pattern, placeholder)
 
